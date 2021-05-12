a5141f7a65523dcd3f5af571e7dcbaefbc2e5d16
hermanjackson <hermanjackson561@gmail.com>
2021-05-12 12:30:46 +0000

    update project

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,29 +7,46 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container_page">
-			<div className="jumbotron" />
-			<div className="col">
-				<div className="row">
-					<div className="card">
-						<img className="card_img" src="http://placehold.jp/200x200.png" />
-						<div className="card_body">
-							<p className="words">just something to say about the card</p>
-							<button className="btn_button" type="button" />
+		<div className="hero">
+			<div className="hero_outer" id="home">
+				<div className="hero__container">
+					<h1 className="hero__heading">guns , guns , guns</h1>
+					<p className="hero__description">something to say</p>
+					<button className="main__btn">
+						<a href="demo">rifles</a>
+					</button>
+				</div>
+			</div>
+
+			<div className="services" id="services">
+				<h1>guns</h1>
+				<div className="services__wrapper">
+					<div className="services__card">
+						<h2>assualt rifles</h2>
+						<p>something else</p>
+						<div className="services__btn">
+							<button className="style_button">something else</button>
 						</div>
 					</div>
-					<div className="card">
-						<img className="card_img" src="http://placehold.jp/200x200.png" />
-						<div className="card_body">
-							<p className="words">just something to say about the card</p>
-							<button className="btn_button" type="button" />
+					<div className="services__card">
+						<h2>hand guns</h2>
+						<p>something else</p>
+						<div className="services__btn">
+							<button className="style_button">something else</button>
 						</div>
 					</div>
-					<div className="card">
-						<img className="card_img" src="http://placehold.jp/200x200.png" />
-						<div className="card_body">
-							<p className="words">just something to say about the card</p>
-							<button className="btn_button" type="button" />
+					<div className="services__card">
+						<h2>snipers</h2>
+						<p>something else</p>
+						<div className="services__btn">
+							<button className="style_button">something else</button>
+						</div>
+					</div>
+					<div className="services__card">
+						<h2>how to get gun permits</h2>
+						<p>something else</p>
+						<div className="services__btn">
+							<button className="style_button">something else</button>
 						</div>
 					</div>
 				</div>
