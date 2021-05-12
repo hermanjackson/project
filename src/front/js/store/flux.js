0cde1baf41da36c3add85f11095a08bc19a706ba
hermanjackson <hermanjackson561@gmail.com>
2021-05-12 17:27:12 +0000

    update

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,16 +2,115 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
+			HandGuns: [
 				{
 					id: "1",
-					decription: "whatever text you want to put in here this jjust a sample",
-					gun: ""
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "sig-saucer 9mm"
 				},
 				{
+					uid: "2",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "berreta 9mm"
+				},
+				{
+					uid: "3",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "spring-field xd 9mm"
+				},
+				{
+					uid: "4",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "Glock 9mm"
+				},
+				{
+					uid: "5",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "sig-saucer 40cal"
+				},
+				{
+					uid: "6",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "38 revolver"
+				},
+				{
+					uid: "7",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: "357 revolver"
+				}
+			],
+			Rifles: [
+				{
 					id: "1",
-					decription: "whatever text you want to put in here this jjust a sample",
-					gun: ""
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "2",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "3",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "4",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "5",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "6",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "7",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				}
+			],
+			ShotGun: [
+				{
+					id: "1",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "2",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "3",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "4",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "5",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "6",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
+				},
+				{
+					uid: "7",
+					Image: "http://placehold.jp/150x150.png",
+					Gun: ""
 				}
 			]
 		},
