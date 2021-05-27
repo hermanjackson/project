78cedf4fb4e00a19be813705805abcbbc21ebf0b
hermanjackson <hermanjackson561@gmail.com>
2021-05-27 14:48:53 +0000

    added user

--- src/front/js/pages/recipes.js
+++ src/front/js/pages/recipes.js
@@ -8,10 +8,33 @@ import "../../styles/recipes.scss";
 
 export const Recipes = () => {
 	return (
-		<div>
-			<div>
-				<h1>hi</h1>
+		<div className="form__container">
+			<div className="form_header">
+				<img className="profile_image" src="http://placehold.jp/150x150.png" />
+				<i className="fas fa-camera" />
 			</div>
+			<form className="form_content">
+				<div className="text__area">
+					<textarea
+						className="user_recipe_ingredients"
+						value="type your ingredients here"
+						name="user_recipe_ingredients"
+						rows="4"
+						cols="50"
+					/>
+					<textarea className="user_recipe" value="list your steps" name="user_recipe" rows="4" cols="50" />
+					<textarea
+						className="user_recipe_description"
+						value="user_recipe_description"
+						name="user_recipe_description"
+						rows="4"
+						cols="50"
+					/>
+					<div className="image">
+						<img src="http://placehold.jp/300x300.png" />
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
