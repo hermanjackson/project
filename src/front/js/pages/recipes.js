ce59b1468a42ab6bea772a4444cabbdac2117f8f
hermanjackson <hermanjackson561@gmail.com>
2021-05-28 00:33:19 +0000

    added photo update

--- src/front/js/pages/recipes.js
+++ src/front/js/pages/recipes.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { Link, Value } from "react-router-dom";
 
 import PropTypes from "prop-types";
@@ -7,11 +7,39 @@ import { Context } from "../store/appContext";
 import "../../styles/recipes.scss";
 
 export const Recipes = () => {
+	const [photo, setPhoto] = useState("");
+	const [profileUrl, setProfileUrl] = useState("http://placehold.jp/150x150.png");
+
+	const fileInput = useRef(null);
+	useEffect(
+		() => {
+			if (photo !== "") {
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					// log to console
+					// logs data:<type>;base64,wL2dvYWwgbW9yZ...
+					setProfileUrl(reader.result);
+				};
+				reader.readAsDataURL(photo);
+			}
+		},
+		[photo]
+	);
 	return (
 		<div className="form__container">
-			<div className="form_header">
-				<img className="profile_image" src="http://placehold.jp/150x150.png" />
-				<i className="fas fa-camera" />
+			<div className="jumbotron">
+				<div className="user__container">
+					<img className="main_img" src="http://placehold.jp/600x300.png" />
+					<img className="profile_image" src={profileUrl} />
+					<h4 className="user_name">user name</h4>
+					<i onClick={() => fileInput.current.click()} className="fas fa-camera" />
+					<input
+						ref={fileInput}
+						onChange={e => setPhoto(e.target.files[0])}
+						type="file"
+						className="invisible"
+					/>
+				</div>
 			</div>
 			<form className="form_content">
 				<div className="text__area">
